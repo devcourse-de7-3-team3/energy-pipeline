20d4e68a8e462317eb2bcdb527668dd5864c0d01
SimHoon <simhoon1023@gmail.com>
2025-11-17 20:52:04 +0900

    Add: airflow/dags/dbt_analytics_nutrient.py

---
+++ airflow/dags/dbt_analytics_nutrient.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "simhoon",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=1),
+}
+
+with DAG(
+    dag_id="dbt_nutrient_daily",
+    default_args=default_args,
+    start_date=datetime(2025, 11, 17),
+    schedule_interval="20 2 * * *",
+    catchup=False,
+    tags=["meal", "dbt", "analytics"],
+) as dag:
+
+    run_dbt_nutrient = BashOperator(
+        task_id="run_dbt_nutrient",
+        bash_command=(
+            "cd /opt/airflow/meal_dbt && "
+            "dbt run -s nutrient"
+        ),
+    )
