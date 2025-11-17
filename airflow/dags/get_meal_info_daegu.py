bd8a242a8f80ae75207c72c2151bb3d8dec529e4
JungEun Park <jungeun121@gmail.com>
2025-11-17 15:47:40 +0900

    고등학교 학교정보만 받아오도록 변경

--- airflow/dags/get_meal_info_daegu.py
+++ airflow/dags/get_meal_info_daegu.py
@@ -18,7 +18,8 @@ def get_code_daegu():
     cur = conn.cursor()
     sql = """
         SELECT DISTINCT atpt_ofcdc_sc_code, sd_schul_code
-        FROM ENERGY_DB.RAW_DATA.SCHOOL_INFO_DAEGU;
+        FROM ENERGY_DB.RAW_DATA.SCHOOL_INFO_DAEGU
+        WHERE SCHUL_NM like '%고등학교';
     """
     cur.execute(sql)
     code_list = [(r[0], r[1]) for r in cur.fetchall()]
