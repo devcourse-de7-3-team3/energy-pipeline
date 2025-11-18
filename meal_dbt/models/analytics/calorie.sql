5eb48166413e9a69161c5aadee75aa7a721e1ba6
JungEun Park <jungeun121@gmail.com>
2025-11-18 13:43:37 +0900

    REFACTOR : 시각화 위해 CALORIE DBT 쿼리 수정

--- meal_dbt/models/analytics/calorie.sql
+++ meal_dbt/models/analytics/calorie.sql
@@ -10,5 +10,16 @@ SELECT
     SCHUL_NM,
     MMEAL_SC_CODE,
     MMEAL_SC_NM,
-    CAL_INFO
+    MLSV_YMD,
+    CASE DAYOFWEEK(MLSV_YMD)
+        WHEN 1 THEN '월'
+        WHEN 2 THEN '화'
+        WHEN 3 THEN '수'
+        WHEN 4 THEN '목'
+        WHEN 5 THEN '금'
+        WHEN 6 THEN '토'
+        WHEN 7 THEN '일'
+    END AS WEEOKDAY_KO,
+    CAL_INFO,
+    TRY_TO_DECIMAL(TRIM(REPLACE(CAL_INFO, 'Kcal', '')), 10, 1) AS CAL_INFO_CLEANED
 FROM {{ source('stg_data', 'MEAL_DIET_INFO_ALL') }}
